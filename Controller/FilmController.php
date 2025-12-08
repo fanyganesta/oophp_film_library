3a93d1c6c617feffae9606fc0054036e75e15415
fanyganesta <fanyganesta@gmail.com>
2025-12-08 11:27:52 +0700

    index all data dengan pencarian dan pagination best practice selesai

--- Controller/FilmController.php
+++ Controller/FilmController.php
@@ -19,34 +19,19 @@
                 $halamanAktif = $request['halaman'];
             }
 
-            $queryAll = "SELECT * FROM $this->table";
-            $rows = $this->conn->query($queryAll);
 
-            $jumlahHalaman = ceil(count($rows)/$limit);
+            $find = "SELECT * FROM $this->table WHERE judul LIKE ? OR deskripsi LIKE ? OR tahunTerbit LIKE ? OR rating LIKE ?";
 
+            $cari = isset($_GET['cari']) ? '%'.$_GET['cari'].'%' : '%%';
             $index = $halamanAktif * $limit - $limit;
-            if(isset($_GET['cari'])){
-                $find = "SELECT * FROM $this->table WHERE judul LIKE ? OR deskripsi LIKE ? OR tahunTerbit LIKE ? OR rating LIKE ?";
-                $cari = '%'.$_GET['cari'].'%';
-
-                $allFinds = $this->conn->query($find, [$cari,$cari,$cari,$cari]);
-                $queryPagination = $find . " LIMIT $index, $limit";
-
-                $rows = $this->conn->query($queryPagination, [$cari,$cari,$cari,$cari]);
-                $jumlahHalaman = ceil(count($allFinds)/$limit);
-            }else{
-                $queryPagination = "SELECT * FROM $this->table LIMIT $index, $limit";
-                $rows = $this->conn->query($queryPagination);
-            }
 
             
+            $allFinds = $this->conn->query($find, [$cari,$cari,$cari,$cari]);
+            $queryPagination = $find . " LIMIT $index, $limit";
 
-            $result = [];
-            foreach($rows as $row){
-                $row += ['halamanAktif' => $halamanAktif, 'jumlahHalaman' => $jumlahHalaman];
-                $result[] = $row;
-            }
-            $rows = $result;
+            $rows = $this->conn->query($queryPagination, [$cari,$cari,$cari,$cari]);
+            $jumlahHalaman = ceil(count($allFinds)/$limit);
+    
             return view('Films/film-list', compact('rows', 'jumlahHalaman', 'halamanAktif'));
         }
 
